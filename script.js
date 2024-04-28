fcac9c84af779039292474051ef43fe96d59367c
Gleisson Braga <bragagleisson@gmail.com>
2024-04-28 12:11:48 -0300

    fix: corrigindo botao do js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const toggleBtn = document.getElementByClassName('nav-mobile-button');
-    const menuItems = document.getElementsByClassName('nav-container');
+    const toggleBtn = document.querySelector('.nav-mobile-button');
+    const menuItems = document.querySelector('.nav-container'); // added dot before 'nav-container'
 
     toggleBtn.addEventListener('click', function() {
         menuItems.classList.toggle('show');
